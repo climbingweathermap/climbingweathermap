d02b4164faaf98e430225cef197fcaf1cc98f23b
fanoway <86997883+fanoway@users.noreply.github.com>
2021-07-16 17:45:58 -0700

    Modal and formatting

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,41 @@
 <template>
-  <h1> {{title}} </h1>
-  <input type="text" ref="name">
-  <button @click='handleClick'>Click Me</button>
+    <h1> {{title}} </h1>
+    <p>
+        <input type="text" ref="name">
+        <button @click='handleClick'>Click Me</button>
+    </p>
+    <Modal />
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-  data(){
-    return{
-      title: 'My first view app '
+    import Modal from './components/Modal.vue'
+    export default {
+        name: 'App',
+        components: {
+            Modal
+        },
+        data() {
+            return {
+                title: 'My first view app '
+            }
+        },
+        methods: {
+            handleClick() {
+                console.log(this.$refs.name)
+                this.$refs.name.classList.add('active')
+                this.$refs.name.focus()
+            }
+        }
     }
-  },
-  methods:{
-    handleClick(){
-      console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus() 
-    }
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-h1{
-  border-bottom:1px  solid #ddd;
-  display: inline-block;
-  padding-bottom: 10px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
