39a2f83c05f2d4dfcc5087f07e26d11ef90f07aa
fanoway <86997883+fanoway@users.noreply.github.com>
2021-07-19 17:02:08 -0700

    Tidy up

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,8 @@
 <template>
-    <footer class="footer border-top border-primary">
-        Copyright ©2021 Fanoway
+    <footer class="footer border-top border-primary navbar">
+        <div class=" me-auto">Copyright ©2021 Fanoway</div>
+        <div class="">
+            Powered by <a href="https://www.weatherapi.com/" title="Weather API">WeatherAPI.com</a></div>
     </footer>
 </template>
 
