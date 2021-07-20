7d3d99e6b1427dc65e2365a84080be9ad14a3a8b
fanoway <86997883+fanoway@users.noreply.github.com>
2021-07-20 07:20:23 -0700

    largeer radius for display purposes

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ LOCATIONS_API= "https://climb-api.openbeta.io/geocode/v1/sectors"
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # Get locations from open beta
-query = {"latlng": "45.482300,-122.558441", "radius": 10}
+query = {"latlng": "45.482300,-122.558441", "radius": 100}
 sectors = requests.get(
     app.config["LOCATIONS_API"], params=query, verify=False
 ).json()
