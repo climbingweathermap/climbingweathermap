374725a58f55d83b2bf54d3b8aa36ed67dc51af8
fanoway <86997883+fanoway@users.noreply.github.com>
2021-07-24 15:08:30 -0700

    fix mailto

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="footer border-top border-primary navbar">
-        <div class=" me-auto">Copyright ©2021 <a href="mailto:<div class=" me-auto">Copyright ©2021 Climbing Weather Map</div>">Climbing Weather Map</a></div>
+        <div class=" me-auto">Copyright ©2021 <a href="mailto:climbingweathermap@gmail.com">Climbing Weather Map</a></div>
         <div class="">
             Powered by <a href="https://www.weatherapi.com/" title="Weather API">WeatherAPI.com</a></div>
     </footer>
