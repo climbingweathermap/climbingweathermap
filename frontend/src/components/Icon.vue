c2201be0b524ad78cc1551557b3227235490dd83
fanoway <86997883+fanoway@users.noreply.github.com>
2021-07-25 23:01:37 -0700

    custom icon

---
+++ frontend/src/components/Icon.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="iconBox">
+        <div>
+            <img :src="summaryIcon" height="32" />
+        </div>
+        <div>
+            <img :src="rainIcon" height="32" />
+        </div>
+        <div>
+            <p>{{temp.toFixed(1)}}</p>
+        </div>
+        <div>
+            <p>{{title}}</p>
+        </div>
+    </div>
+</template>
+
+<script type="text/javascript">
+    export default {
+        name: "Icon",
+        props: {
+            title: String,
+            summaryIcon: String,
+            rainIcon: String,
+            temp: Number,
+        },
+        methods: {
+            toFixed: (value, precision) => {
+                var power = Math.pow(10, precision || 0);
+                return String(Math.round(value * power) / power);
+            }
+        },
+    }
+</script>
+
+<style>
+    .iconBox {
+        display: flex;
+        background: var(--bs-dark2);
+        color: var(--bs-light);
+        padding: 5px;
+        max-height: 32px;
+        overflow: hidden;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        border-radius: 5px;
+
+    }
+
+    .iconBox p {
+        margin: 0;
+        margin-left: 5px;
+        max-width: 80px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap
+    }
+</style>
