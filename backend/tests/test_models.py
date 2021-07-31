100fa38d20e2ea0a6e27ff0413cbd680a8362d49
fanoway <86997883+fanoway@users.noreply.github.com>
2021-07-31 09:20:12 -0700

    More Tests

--- backend/tests/test_models.py
+++ backend/tests/test_models.py
@@ -100,5 +100,38 @@ def test_weather_to_dict_withcall(my_location, my_weather):
     }
 
 
-def test_get_weather(my_weather):
-    """Test weather api call using mock"""
+def test_get_precip_error(my_weather_nocall):
+    """test weather get precip raises an error if
+    weather data is not collected first."""
+    with pytest.raises(WeatherNotCollectedError):
+        my_weather_nocall.get_precip([1627599277, 1627772077])
+
+
+def test_get_precip(my_weather):
+    pass
+
+
+@pytest.mark.parametrize(
+    "rain,total", [({"1h": 5, "2h": "3", "3h": 2}, 10), ("2", 2)]
+)
+def test_sum_all_rain(rain, total):
+    """Test sum all rain static method."""
+    assert Weather.sum_all_rain(rain) == total
+
+
+def test_WeatherAPIError_response():
+    """Check string output of WeatherAPIError."""
+    response_str = "Source of error: unknown"
+    assert str(WeatherAPIError("unknown")) == response_str
+
+
+def test_WeatherNotCollectedError_response():
+    """Check string output of WeatherNotCollectedError."""
+    response_str = "Weather must be collected from the API first"
+    assert str(WeatherNotCollectedError()) == response_str
+
+
+def test_InvalidLatLng_response():
+    """Check string output of InvalidLatLng."""
+    response_str = "Invalid Coordinates, lat/long = [45, -129]"
+    assert str(InvalidLatLng([45, -129])) == response_str
