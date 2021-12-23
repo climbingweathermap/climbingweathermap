7f42427ecd7c34c946cf4a6b916eb650edb0763c
Michael Ardron <climbingweathermpa@gmail.com>
2021-12-22 22:10:34 -0800

    try to fix mobile issues on footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,8 +1,10 @@
 <template>
     <footer class="footer border-top border-primary navbar">
         <div class=" me-auto">Copyright ©2021 <a href="mailto:climbingweathermap@gmail.com">Climbing Weather Map</a></div>
-        <div class="">
-            Powered by <a href="https://openweathermap.org/" title="Weather API">OpenWeatherMap.org</a> <a href="https://www.digitalocean.com/?refcode=a521527fcc17&utm_campaign=Referral_Invite&utm_medium=Referral_Program&utm_source=badge"><img src="https://web-platforms.sfo2.digitaloceanspaces.com/WWW/Badge%202.svg" alt="DigitalOcean Referral Badge" /></a></div>
+        <div class="me-auto">
+            Powered by <a href="https://openweathermap.org/" title="Weather API">OpenWeatherMap.org</a>
+            <a href="https://www.digitalocean.com/?refcode=a521527fcc17&utm_campaign=Referral_Invite&utm_medium=Referral_Program&utm_source=badge"><img src="https://web-platforms.sfo2.digitaloceanspaces.com/WWW/Badge%202.svg" alt="DigitalOcean Referral Badge" /></a>
+        </div>
     </footer>
 </template>
 
